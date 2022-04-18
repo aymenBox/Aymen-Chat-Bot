908c539f61b70c25c754ec7105744c433f28f2ad
aymenBox <boxaymen7@gmail.com>
2022-04-18 14:22:46 +0100

    creating ChatContainer.vue and making chat example

---
+++ src/components/ChatContainer.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="q-pa-md">
+        <q-infinite-scroll @load="onLoad" reverse :offset="-1">
+            <template v-slot:loading>
+                <div class="row justify-center q-my-md">
+                    <q-spinner color="primary" name="dots" size="40px" />
+                </div>
+            </template>
+            <div id="MyChatMessageContainer">
+                <q-chat-message name="me" avatar="https://cdn.quasar.dev/img/avatar1.jpg" :text="['hey, how are you?']"
+                    sent />
+                <q-chat-message name="Jane" avatar="https://cdn.quasar.dev/img/avatar2.jpg"
+                    :text="['doing fine, how r you?']" />
+            </div>
+
+        </q-infinite-scroll>
+        <!-- this q-input is for writing messages -->
+        <q-input label="Write a message" >
+            <template v-slot:after>
+                <q-btn round dense flat icon="send" />
+            </template>
+        </q-input>
+    </div>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+    name: 'ChatContainer',
+    setup() {
+        //set ScrollerOffset to -1 to prevent loading on initial load
+        var ScrollerOffset = -1;
+
+        return {
+            ScrollerOffset,
+            onLoad(index, done) {
+                //ToDo: add loading spinner
+            }
+        }
+    }
+})
+</script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,16 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="">
+    <ChatContainer></ChatContainer>
+
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
+import ChatContainer from 'src/components/ChatContainer.vue'
 
 export default defineComponent({
-  name: 'IndexPage'
+  name: "IndexPage",
+  components: { ChatContainer }
 })
 </script>
