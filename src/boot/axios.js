abe8f3922d772e8fa579924b48e07ffeee742e6e
aymenBox <boxaymen7@gmail.com>
2022-04-21 16:51:11 +0100

    changing the api url

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,7 +1,7 @@
 import { boot } from 'quasar/wrappers'
 import axios from 'axios'
 
-const api = axios.create({ baseURL: 'http://localhost:5000' })
+const api = axios.create({ baseURL: 'https://aymenchatbot.herokuapp.com' })
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
